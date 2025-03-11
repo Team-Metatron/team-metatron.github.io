148dbbec01724fb305c7d12d42068d9a9fe9629d
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2025-03-11 14:24:38 +0530

    uncomment LandingPage route in App.js to enable navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Index from "views/Index.js";
-// import LandingPage from "views/examples/LandingPage.js";
+import LandingPage from "views/examples/LandingPage.js";
 // import RegisterPage from "views/examples/RegisterPage.js";
 // import ProfilePage from "views/examples/ProfilePage.js";
 
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/" render={(props) => <Index {...props} />} />
-        {/* <Route path="/landing" render={(props) => <LandingPage {...props} />} /> */}
+        <Route path="/landing" render={(props) => <LandingPage {...props} />} />
         {/* <Route
         path="/register"
         render={(props) => <RegisterPage {...props} />}
